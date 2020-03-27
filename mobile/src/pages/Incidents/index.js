24d97ea150f0e739785cfae73d626a694f90d456
maxandrade7 <maxandrade909@gmail.com>
2020-03-27 17:38:24 -0300

    Mobile da aplicação

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -63,7 +63,6 @@ export default function Incidents(){
         data={incidents}
         style={styles.incidentList}
         keyExtractor={incident=> String(incident.id)}
-        //showsVerticalScrollIndicator={false}
         onEndReached={loadIncidents}
         onEndReachedThreshold={0.2}
         renderItem={({item: incident})=> (
